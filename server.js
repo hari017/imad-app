a0677f082567552a8416a5c1b87459758d33c32c
hari017 <30820833+hari017@users.noreply.github.com>
2017-08-22 19:27:22 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,32 +1,32 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool = require('pg'),Pool;
+
+var config = {
+    user: 'thari0016',
+    database: thari0016,
+    host: db-thari0016-93858,
+    port: '5432',
+    password: process.env.DB_PASSWORD
+};
 
 var app = express();
 app.use(morgan('combined'));
 
-app.get('/',function (req,res) {
-    res.sendFile(path.join(__dirname, 'ui' , 'index.html'));
+function createTemplate (data) {
+res.sendFile(pack.join(_dirname, 'ui', 'index.html'));
 });
 
-app.get('/ui/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+function hash (input)
+app.get('/hash/: input', function(req,res) {
+    var hashedString = hash(res.params.input);
+    res.send(hashedString);
 });
 
-
-app.get('/ui/main.js', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
-});
-
-app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
-});
-
-
-// Do not change port, otherwise your app won't run on IMAD servers
-// Use 8080 only for local development if you already have apache running on 80
-
-var port = 80;
-app.listen(port, function () {
-  console.log(`IMAD course app listening on port ${port}!`);
-});
+var pool = new Pool(config);
+app.get('/test-db', function (req,res) {
+    
+    pool.query 
+    
+    
